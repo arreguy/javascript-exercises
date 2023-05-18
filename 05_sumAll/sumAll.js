d2ab13fd87360e14ed358ac4ca2ebdf3529e10d0
arreguy <brenoarreguy@gmail.com>
2023-05-18 10:29:57 -0300

    Add solution to 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+    if (typeof(y) !== 'number' || typeof(x) !== 'number' || y < 0 || x < 0) {
+        return "ERROR";
+    }
+    let sum = 0;
+    if(x > y) {
+        high = x;
+        low = y;
+    } else {
+        high = y;
+        low = x;
+    }
+    for(let i = low; i < high + 1; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
