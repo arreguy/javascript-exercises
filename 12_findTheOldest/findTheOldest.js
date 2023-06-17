34fb1d32a8769de8d8e24be8526257bbed86982f
arreguy <brenoarreguy@gmail.com>
2023-06-17 19:37:43 -0300

    Add solution to 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldestPerson, currentPerson) => {
+        if (!currentPerson.yearOfDeath) {
+            currentPerson.yearOfDeath = new Date().getFullYear();
+        }
+        return (currentPerson.yearOfDeath - currentPerson.yearOfBirth) > (oldestPerson.yearOfDeath - oldestPerson.yearOfBirth) ? currentPerson : oldestPerson;
+    }, people[0]);
 };
 
 // Do not edit below this line
