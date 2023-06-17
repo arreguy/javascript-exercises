f19401685abd4206dd4df6ca73ed2e23d8a113f9
arreguy <brenoarreguy@gmail.com>
2023-06-17 18:21:34 -0300

    Add solution to 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    return string.toLowerCase().replace(/[^a-z]/g, "") === string.toLowerCase().replace(/[^a-z]/g, "").split("").reverse().join("");
 };
+
+palindromes('Racecar!')
 
 // Do not edit below this line
 module.exports = palindromes;
