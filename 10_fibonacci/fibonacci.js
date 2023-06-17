b3ff59f14be5430bcd21e641da83ee056b1f1599
arreguy <brenoarreguy@gmail.com>
2023-06-17 18:37:10 -0300

    Add solution to 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(x) {
+    if (x < 0) return "OOPS"
+    if (x == 0) return 0;
+    let fibonacci_array = [1, 1]
+    for(let i = 2; i < x; i++) {
+        fibonacci_array.push(fibonacci_array[i - 1] + fibonacci_array[i - 2]);
+    }
+    return fibonacci_array[x - 1];
 };
 
 // Do not edit below this line
